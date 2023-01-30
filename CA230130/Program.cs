c177c419d4c5cc2809fe93621258aa8b768f8883
szilagyi.gergely <szilagyi.gergely@28-09>
2023-01-30 08:30:13 +0100

    Add project files.

---
+++ CA230130/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CA230130
+{
+    internal class Program
+    {
+        static string GetHashValueString(byte[] hv)
+        {
+            string hvs = string.Empty;
+            for (int i = 0; i < hv.Length; i++)
+            {
+                hvs += $"{hv[i]:X2}";
+                if ((i + 1) % 4 == 0) hvs += " ";
+            }
+            return hvs;
+        }
+        static void Main(string[] args)
+        {
+            Byte[] pass = System.Text.Encoding.UTF8.GetBytes("Pass");
+
+            SHA256 sha256 = SHA256.Create();
+
+            byte[] data = sha256.ComputeHash(pass);
+
+            string HashString = GetHashValueString(data);
+
+            Console.WriteLine(HashString);
+        }
+    }
+}
